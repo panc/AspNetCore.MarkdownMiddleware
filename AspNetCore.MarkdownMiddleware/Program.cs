060ab28a8f9f9507d04e269143d49f28df5105c7
Christoph Pangerl <christoph.pangerl@gmail.com>
2024-02-10 20:50:27 +0100

    Updated to .NET 8

--- AspNetCore.MarkdownMiddleware/Program.cs
+++ AspNetCore.MarkdownMiddleware/Program.cs
@@ -1,20 +1,54 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using AspNetCore.MarkdownMiddleware;
 
-namespace AspNetCore.MarkdownMiddleware
+[assembly: CLSCompliant(false)]
+
+var builder = WebApplication.CreateBuilder();
+
+// =============================
+// Add services to the container
+// =============================
+
+// setup markdown middleware
+builder.Services.AddMarkdown(config =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    config.BasePath = "md";
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    config.AddMarkdownFolder("/Samples");
+    config.AddMarkdownFolder("/Others");
+});
+
+builder.Services.AddRazorPages();
+
+
+// =====================
+// Configure Application
+// =====================
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+// must be configured before app.UseRouting()!
+app.UseMarkdown();
+
+app.UseRouting();
+app.UseStaticFiles();
+
+app.MapRazorPages();
+
+
+// ===============
+// Run Application
+// ===============
+
+app.Run();
